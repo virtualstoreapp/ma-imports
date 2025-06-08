420595f84880028a9558bb590ad726cf852bb922
Bruno Rodrigo <bnorodrigo@gmail.com>
2025-06-08 19:25:57 +0100

    Update Products (#50)
    * Add sweatshirts
    
    * Add dress shirt
    
    * Update snapshot tests

--- tests/catalog.test.js
+++ tests/catalog.test.js
@@ -40,7 +40,7 @@ const asserts = async (expectedHeading, expectedCount) => {
 
 const assertAllProducts = async () => {
   const expectedHeading = "Novidades";
-  const expectedCount = 146;
+  const expectedCount = 154;
   await asserts(expectedHeading, expectedCount);
 };
 
@@ -121,6 +121,14 @@ const selectTshirtsPoloMan = async () => {
   await asserts(expectedHeading, expectedCount);
 };
 
+const selectDressShirtsMan = async () => {
+  const expectedHeading = "Camisetas Sociais Masculina";
+  const expectedCount = 5;
+  await selectClothingManSubcategory();
+  await selectMenuOption('dress-shirts-man');
+  await asserts(expectedHeading, expectedCount);
+};
+
 const selectTankTopCasualMan = async () => {
   const expectedHeading = "Regatas Casuais Masculina";
   const expectedCount = 3;
@@ -179,7 +187,7 @@ const selectCapsMan = async () => {
 
 const selectSweatshirtMan = async () => {
   const expectedHeading = "Blusas Masculina";
-  const expectedCount = 3;
+  const expectedCount = 6;
   await selectClothingManSubcategory();
   await selectMenuOption('sweatshirts-man');
   await asserts(expectedHeading, expectedCount);
@@ -227,6 +235,10 @@ describe('Catalog', () => {
 
     it('renders final subcategory "Camisetas Polo Masculina" correctly on desktop', async () => {
       await selectTshirtsPoloMan();
+    });
+
+    it('renders final subcategory "Camisetas Sociais Masculina" correctly on desktop', async () => {
+      await selectDressShirtsMan();
     });
 
     it('renders final subcategory "Regatas Casuais Masculina" correctly on desktop', async () => {
@@ -298,6 +310,10 @@ describe('Catalog', () => {
       await selectTshirtsPoloMan();
     });
 
+    it('renders final subcategory "Camisetas Sociais Masculina" correctly on desktop', async () => {
+      await selectDressShirtsMan();
+    });
+
     it('renders final subcategory "Regatas Casuais Masculina" correctly on desktop', async () => {
       await selectTankTopCasualMan();
     });
@@ -343,6 +359,7 @@ describe('Catalog', () => {
       "tshirts-casual-man": [{ name: "[0103250820] Tshirt Casual Man", price: 89.90 }],
       "tshirts-dryfit-man": [{ name: "[2703251659] Tshirt Dryfit Man", price: 59.90 }],
       "tshirts-polo-man": [{ name: "[1603250851] Tshirt Polo Man", price: 59.90 }],
+      "dress-shirts-man": [{ name: "[0807251831] Dress Shirt Man", price: 139.90 }],
       "tank-top-casual-man": [{ name: "[1903251705] Tank Top Casual Man", price: 39.90 }],
       "tank-top-dryfit-man": [{ name: "[0703251715] Tank Top Dryfit Man", price: 59.90 }],
       "shorts-sweatshorts-man": [{ name: "[2003250848] Short Basic Man", price: 69.90 }],
@@ -381,6 +398,7 @@ describe('Catalog', () => {
       const productItems = Array.from(document.querySelectorAll('#product-list .product-item'));
       const productNames = productItems.map(item => item.querySelector('h3').textContent);
       expect(productNames).toEqual([
+        "[0807251831] Dress Shirt Man",
         "[0106250956] Cap Man",
         "[0106250834] Socks Man",
         "[0106250800] Sweatshirt Man",
