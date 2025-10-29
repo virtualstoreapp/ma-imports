ace466d33645625bb4b0f60a5fb7c3baf4ec0a2e
Bruno Rodrigo <bnorodrigo@gmail.com>
2025-10-29 20:14:47 +0000

    Update products (#75)
    * Update products
    
    * Fix snapshot

--- tests/utils/catalogAsserts.js
+++ tests/utils/catalogAsserts.js
@@ -24,7 +24,7 @@ const asserts = async (expectedHeading, expectedCount) => {
 
 const assertAllProducts = async () => {
   const expectedHeading = "Novidades";
-  const expectedCount = 234;
+  const expectedCount = 241;
   await asserts(expectedHeading, expectedCount);
 };
 
